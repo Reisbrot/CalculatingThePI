73474dc89da8c37b6e0177adebcbc66d36b758ce
Reisbrot <cornigruen@gmail.com>
2018-05-20 18:08:55 +0200

    Calculates PI to the 7th decimal point in under 30min

---
+++ WithJava/src/withjava/PI.java
@@ -0,0 +1,16 @@
+package withjava;
+
+public class PI {
+    public static void main(String[] args) {
+        double fourthOfPi = 1;
+        int sign = 0;                       /*0 entspricht negativem Vorzeichen, 1 positivem.*/
+        for(double i = 3; fourthOfPi*4 > 2.141; i+=2){
+            if(sign%2 == 0)
+                fourthOfPi = fourthOfPi - (1/i);
+            else
+                fourthOfPi = fourthOfPi + (1/i);
+            sign++;
+            System.out.println(fourthOfPi * 4);
+        }
+    }
+}
